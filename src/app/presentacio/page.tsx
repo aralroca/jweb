8734b2da560476c99cf9ab739358b90931786971
Aral Roca <aral-rg@hotmail.com>
2023-05-09 00:04:50 +0200

    fix

--- src/app/presentacio/page.tsx
+++ src/app/presentacio/page.tsx
@@ -3,7 +3,7 @@ import DisplayPDF from "../components/DisplayPDF";
 export default function Presentation() {
   return (
     <div className="content flat">
-      <DisplayPDF name="presentacio_cat.pdf" pdfLink="/pdfs/presentacio/CAT.PDF" />
+      <DisplayPDF name="presentacio_cat.pdf" pdfLink="/pdfs/presentacio/CAT.pdf" />
     </div>
   )
 }
