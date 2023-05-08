3ebba753d6eb02f378d0681532e4edf52d299e82
Aral Roca <aral-rg@hotmail.com>
2023-05-08 14:56:27 +0200

    add default data

---
+++ src/app/constants/default-data.ts
@@ -0,0 +1,211 @@
+export const projectCategories = [
+  { id: 1, name: 'PLANEJAMENT URBANÍSTIC', order: 1 },
+  { id: 2, name: 'PROJECTES URBANS', order: 2 },
+  { id: 3, name: 'DISSENY DE MOBILIARI', order: 3 },
+  { id: 4, name: 'PROJECTES DE REHABILITACIÓ', order: 4 },
+  { id: 5, name: 'PROJECTES D\'EDIFICACIÓ', order: 5 },
+];
+
+export const projectes = [
+  { id: 3, name: '1986. Pla Especial del Segon Cinturó', category: 1 },
+  { id: 2, name: '1986. Pla Especial del Marge Esquerre del Riu Besòs', category: 1 },
+  { id: 4, name: '1992. Pla Especial de l\'Avinguda de l\'Onze de Setembre', category: 1 },
+  { id: 26, name: '2008. CEIP Ignasi Iglesias', category: 5 },
+  { id: 5, name: '1995. Pla Especial de l\'Entorn del Parc Central', category: 1 },
+  { id: 6, name: '2007. Urbanització dels Jardins del Casc Antic, Pou de la Figuera', category: 2 },
+  { id: 7, name: '2009. Projecte de Reforma de la Via Laietana', category: 2 },
+  { id: 8, name: '1995. Passeig Marítim de la Barceloneta', category: 2 },
+  { id: 9, name: '1994. Passeig de Sant Antoni', category: 2 },
+  { id: 10, name: '2002. Parc Central de Mataró', category: 2 },
+  { id: 11, name: '2000. Rambla del Raval', category: 2 },
+  { id: 12, name: '2005. Remodelació dels Entorns del Mercat de Santa Caterina, Avinguda Francesc Cambó', category: 2 },
+  { id: 13, name: '1990. Banc “Levit”', category: 3 },
+  { id: 14, name: '1994. Jardinera “Tram”', category: 3 },
+  { id: 15, name: '1995. Columna d\'enllumenat “Kanya”', category: 3 },
+  { id: 16, name: '1998. Columna d\'enllumenat “Ful”', category: 3 },
+  { id: 17, name: '1997. Escultura “Arbre Paer”', category: 3 },
+  { id: 18, name: '1989. Rehabilitació de Can Cisteré', category: 4 },
+  { id: 19, name: '1994. Rehabilitació de l\'edifici d\'habitatges a les Rambles 68', category: 4 },
+  { id: 20, name: '2004. Projecte de Rehabilitació de la Masia Can Xic Dalmau', category: 4 },
+  { id: 21, name: '1999. Teatre Clavé de Tordera', category: 5 },
+  { id: 22, name: '2008. Concurs Obert de l\'Edifici Polivalent 2 del Campus de Cappont', category: 5 },
+  { id: 23, name: '2008. Concurs del CEIP de Palafrugell', category: 5 },
+  { id: 24, name: '2003. Edifici de 12 habitatges al carrer de Riera Alta 15', category: 5 },
+  { id: 25, name: '2008. Caserna de la Guàrdia Urbana de Gràcia', category: 5 },
+  { id: 27, name: '1987. Projecte Cubiculum', category: 4 },
+  { id: 28, name: '2007. Nova Bocana del Port de Barcelona', category: 2 },
+  { id: 29, name: '2008. Concurs del Centre Direccional del Prat Nord', category: 1 },
+  { id: 30, name: '2001. Escossell Rámla', category: 3 },
+  { id: 31, name: '2003. Casa Boned-Ducase', category: 4 },
+  { id: 32, name: '2000. Rehabilitació per a Antonio Pizza', category: 4 },
+  { id: 33, name: '2008. Espai Polisportiu de Can Zam', category: 5 },
+  { id: 34, name: '2009. CEIP Riu Segre', category: 5 },
+  { id: 35, name: '2009. Parc Aerospacial de Viladecans', category: 2 },
+  { id: 36, name: '2009. Concurs IES Puigcerdà', category: 5 },
+  { id: 37, name: '2008. Concurs CEIP Pineda', category: 5 },
+  { id: 38, name: '2006. Barri de Sant Genís dels Agudells', category: 1 },
+  { id: 39, name: '2008. Concurs de l\'Illa Central de Badalona', category: 2 },
+  { id: 40, name: '2008. Passeig Marítim 3ª fase', category: 2 },
+  { id: 41, name: '2010. Columnes Nerium', category: 3 },
+];
+
+export const concursos = [
+  {
+    src: 'images/concursos/PANELL 2 A3-300dpi.jpg',
+    title: 'Concurs d\'Ordenació de l\'Illa Cental de Badalona',
+    description: '<p>Ajuntament de Badalona (Barcelona)</p><p>Jordi Henrich, Jaume Artiques, arquitectes</p><p>Seleccionats per la segona fase</p><p>2008-09</p>'
+  },
+  {
+    src: 'images/concursos/2 panell 2.jpg',
+    title: 'Concurs del Centre Direccional del Prat Nord',
+    description: '<p>Ajuntament del Prat de Llobregat, Consorci del Prat Nord, INCASOL</p><p>H+N+S Landschapsarchitectes - AAUP, Jordi Romero i ass. SLP - Jansana, De la Villa, de Paaw, arq. SL, - Jordi Hemrich, Jaume Artigues, arq.</p><p>2008-09</p>'
+  },
+  {
+    src: 'images/concursos/Imatge_2.jpg',
+    title: 'Concurs d\'idees NumaPort, Neuchâtel (Suïssa)',
+    description: '<p>Ville de Neuchâtel, République de Neuchâtel</p><p>Jordi Henrich, Isabel Bennàssar i Jamue Artigues, arq.</p><p>2011-12</p>'
+  },
+  {
+    src: 'images/concursos/G-1.jpg',
+    title: 'Concurs d\'Avantprojecte d\'adquació de la Riera d\'Alella',
+    description: '<p>Ajuntament d\'Alella (Barcelona)</p><p>Jordi Henrich i Jaume Artigues, arquitectes</p><p>Selecció prèvia del concurs d\'avantprojectes</p><p>2007</p>'
+  },
+  {
+    src: 'images/concursos/lamina 3.jpg',
+    title: 'Concurs d\'idees de l\'Ordenació de l\'àrea d\'equipaments de Can Xarau',
+    description: '<p>Ajuntament de Cerdanyola del Vallés (Barcelona)</p><p>Jordi Henrich i Jaume Artigues, arquitectes.</p><p>2010</p>'
+  },
+  {
+    src: 'images/concursos/proposta grafica 3.jpg',
+    title: 'Concurs del Institut Escola de Puigcerdà (Cerdanya)',
+    description: '<p>GISA, Direcció General de Centres Docents</p><p>Jordi Hemrich, Jaume Artigues i Pere Riera (GRECCAT SL), arquitectes</p><p>Finalistes. Tercers classificats</p><p>2009</p>'
+  },
+  {
+    src: 'images/concursos/Render 2 CEIP PALAFRUGELL _Page_3.jpg',
+    title: 'Concurs del Centre d\'E. Infantil i Primària Palafrugell',
+    description: '<p>GISA, Direcció General de Centres Docents</p><p>Jordi Henrich, Jaume Artigues, Pere Riera (GRECCAT SL)</p><p>Segons classificats del Concurs de dues voltes</p><p>2008</p>'
+  },
+  {
+    src: 'images/concursos/Vista_01 mini.jpg',
+    title: 'Concurs dper a la construcció del CEE Horitzó a Pineda (Barcelona)',
+    description: '<p>GISA, Direcció General de Centres Docents</p><p>Jordi Henrich, Jaume Artigues, Pere Riera (GRECCAT SL)</p><p>Finalistes del concurs Obert, Tercers classificats</p><p>2008</p>'
+  },
+  {
+    src: 'images/concursos/FINAL INT final.jpg',
+    title: 'Concurs d\'idees per a la construcció de l\'Edifici Polivalent 2 al Campus de Cappont de la Universitat de Lleida',
+    description: '<p>Jordi Hewnrich, Jaume Artigues, Inés Aubert, arquitectes.</p><p>2008</p>'
+  }
+];
+
+export const homeImages = [
+  {
+    src: 'images/portada/11.File0015.jpg',
+    title: 'Passeig Marítim de la Barceloneta (1er Sector)',
+    description: '<p>Ajuntament Barcelona, Ministerio de Obras Públicas y Medio Ambiente</p><p>Jaume Artigues, Jordi Henrich, Miquel Roig, Olga Tarrasó, Anna Mª Castañeda</p><p>Diploma d\'Enginyeria Civil dels Premis Construmat 1997</p><p>Obra Finalista de la “IV Bienal de Arquitectura de España 1996”, Madrid 1997</p><p>Premi FAD 1996, apartat d\'Espais Exteriors</p><p>Obra Finalista del Premi Rosa Barba, de la I Biennal Europea del Paisatge, 2000</p><p>Obra Finalista dels Premis Dècada, 2005</p><p>1992-95</p>',
+  },
+  {
+    src: 'images/portada/12. 05 midi.jpg',
+    title: 'Passeig Marítim de la Barceloneta (1er Sector)',
+    description: '<p>Ajuntament Barcelona, Ministerio de Obras Públicas y Medio Ambiente</p><p>Jaume Artigues, Jordi Henrich, Miquel Roig, Olga Tarrasó, Anna Mª Castañeda</p><p>Menció Honorífica al Premi “Architecti 1993”, Lisboa, 1993.</p><p>Diploma d\'Enginyeria Civil dels Premis Construmat 1997</p><p>Obra Finalista de la “IV Bienal de Arquitectura de España 1996”, Madrid 1997</p><p>Premi FAD 1996, apartat d\'Espais Exteriors.</p><p>Obra Finalista dels Premis Dècada, 2005</p><p>1992-95</p>',
+  },
+  {
+    src: 'images/portada/13 Foto general mini.jpg',
+    title: 'Rambla del Raval',
+    description: '<p>Ajuntament de Barcelona, Promoció Ciutat Vella SA.</p><p>Jaume Artigues, Pere Cabrera, arqtes, Albert Ribó, arqte tècnic</p><p>Obra Seleccionada per la II Biennal Europea del Paisatge, 2001.</p>',
+  },
+  {
+    src: 'images/portada/13 Park 3.jpg',
+    title: 'Parc Central de Mataró',
+    description: '<p>Ajuntament de Mataró (Barcelona), PUMSA.</p><p>ARR, arquitectes, s.c.p.”, Jaume Artigues, Miquel Roig, Jordi Romero</p><p>Primer Premi del Concurs d\'Idees i encàrrec del Projecte, 1995</p><p>Obra Seleccionada de les II i III Biennals Europees del Paisatge, 2001</p><p>Obra Finalista dels Premis d\'Espais i Equipaments de Qualitat, 2002</p><p>Premi de la Primera Triennal d\'arquitectura del Maresme, 2004</p><p>1995-02</p>',
+  },
+  {
+    src: 'images/portada/13. File0012.jpg',
+    title: 'Passeig Marítim de la Barceloneta (1er Sector)',
+    description: '<p>Ajuntament Barcelona, Ministerio de Obras Públicas y Medio Ambiente</p><p>Jaume Artigues, Jordi Henrich, Miquel Roig, Olga Tarrasó, Anna Mª Castañeda</p><p>Diploma d\'Enginyeria Civil dels Premis Construmat 1997.</p><p>Finalista de la “IV Bienal de Arquitectura de España 1996”, Madrid 1997</p><p>Premi FAD 1996, apartat d\'Espais Exteriors.</p><p>Finalista del Premi Rosa Barba, de la I Biennal Europea del Paisatge, 2000</p><p>Finalista dels Premis Dècada, 2005</p><p>1992-95</p>',
+  },
+  {
+    src: 'images/portada/1718_2 copia.jpg',
+    title: '12 habitatges als carrers de Riera Alta 15 i del Carme 104',
+    description: '<p>Barcelona (Districte de Ciutat Vella)</p><p>Jaume Artigues i Vidal, col.laborador Stefano Cortellaro</p><p>Seleccionat pel Concurs restringit i encàrrec del projecte</p><p>2001-03</p>',
+  },
+  {
+    src: 'images/portada/1926 10 copia 4.jpg',
+    title: 'Centre d\'E. Infantil i Primària Ignasi Iglesias',
+    description: '<p>GISA, D. G. de Centres Docents de la Generalitat, Cornellà de Llobregat (Barcelona)</p><p>Jaume Artigues i Vidal i Pere Riera i Jané , arquitectes</p><p>2003-07</p>',
+  },
+  {
+    src: 'images/portada/1926 4 copia 4.jpg',
+    title: 'Centre d\'E. Infantil i Primària Ignasi Iglesias',
+    description: '<p>GISA, Direcció general de Centres Docents de la Generalitat, Cornellà de Llobregat (Barcelona)</p><p>Jaume Artigues i Vidal i Pere Riera i Jané , arquitectes</p><p>2003-07</p>',
+  },
+  {
+    src: 'images/portada/1932 6 copia 4.jpg',
+    title: 'Edifici de la Guàrdia Urbana del Districte de Gràcia',
+    description: '<p>Ajuntament de Barcelona, BIMSA (Districte de Gràcia)</p><p>Jaume Artigues i Vidal i Pere Riera i Jané , arquitectes</p><p>2002-08</p>',
+  },
+  {
+    src: 'images/portada/1998 Ful.jpg',
+    title: 'Columna d\'enllumenat “Ful”',
+    description: '<p>Escofet ®</p><p>Jaume Artigues, Pere Cabrera, arquitectes</p><p>1998-99</p>',
+  },
+  {
+    src: 'images/portada/6 aeria Parc Central.jpg',
+    title: 'Nou Parc Central de Mataró',
+    description: '<p>Ajuntament de Mataró (Barcelona), PUMSA.</p><p>ARR, arquitectes, s.c.p.”, Jaume Artigues, Miquel Roig, Jordi Romero</p><p>Primer Premi del Concurs d\'Idees i encàrrec del Projecte, 1995</p><p>Obra Seleccionada de les II i III Biennals Europees del Paisatge, 2001</p><p>Obra Finalista dels Premis d\'Espais i Equipaments de Qualitat, 2002</p><p>Premi de la Primera Triennal d\'arquitectura del Maresme, 2004</p><p>1995-02</p>',
+  },
+  {
+    src: 'images/portada/7 2010 Tavisa detall.jpg',
+    title: 'Rambla del Raval',
+    description: '<p>Ajuntament de Barcelona, Promoció Ciutat Vella SA.</p><p>Jaume Artigues, Pere Cabrera, arqtes, Albert Ribó, arqte tècnic</p><p>Obra Seleccionada per la II Biennal Europea del Paisatge, 2001.</p><p>1995-00</p>',
+  },
+  {
+    src: 'images/portada/8 .080.jpg',
+    title: 'Nou Parc Central de Mataró',
+    description: '<p>Ajuntament de Mataró (Barcelona), PUMSA.</p><p>ARR, arquitectes, s.c.p.”, Jaume Artigues, Miquel Roig, Jordi Romero</p><p>Primer Premi del Concurs d\'Idees i encàrrec del Projecte, 1995</p><p>Obra Seleccionada de les II i III Biennals Europees del Paisatge, 2001</p><p>Obra Finalista dels Premis d\'Espais i Equipaments de Qualitat, 2002</p><p>Premi de la Primera Triennal d\'arquitectura del Maresme, 2004 </p><p>1995-02</p>',
+  },
+  {
+    src: 'images/portada/APAER3.jpg',
+    title: 'Escultura “Arbre Paer”, El Canyeret (Lleida)',
+    description: '<p>Escultura commemorativa del 800 aniversari de la Paeria de Lleida</p><p>Benet Rossell, Cristina Giorgi, artistes, Jaume Artigues, arquitecte</p><p>Primer Premi del Concurs d\'Idees 1997</p><p>1997-98</p>',
+  },
+  {
+    src: 'images/portada/Conjunt 4.JPG',
+    title: 'Reforma i Ampliació del CEIP Riu Segre',
+    description: '<p>GISA, Direcció general de Centres Docents de la Generalitat, (Lleida)</p><p>Jaume Artigues i Vidal i Pere Riera i Jané , arquitectes</p><p>2005-10</p>',
+  },
+  {
+    src: 'images/portada/Conjunt 5.JPG',
+    title: 'Reforma i Ampliació del CEIP Riu Segre',
+    description: '<p>GISA, Direcció general de Centres Docents de la Generalitat, (Lleida)</p><p>Jaume Artigues i Vidal i Pere Riera i Jané , arquitectes</p><p>2005-10</p>',
+  },
+  {
+    src: 'images/portada/File0001.jpg',
+    title: 'Banc “Levit” ',
+    description: '<p>Escofet ®</p><p>Jaume Artigues, arquitecte</p><p>1990-91</p>',
+  },
+  {
+    src: 'images/portada/Sala3.JPG',
+    title: 'Teatre Clavé de Tordera',
+    description: '<p>Ajuntament de Tordera (Barcelona)</p><p>“ARR, arquitectes, s.c.p.”: Jaume Artigues, Miquel Roig, Jordi Romero</p><p>1988-99</p>',
+  },
+  {
+    src: 'images/portada/tram.jpg',
+    title: 'Jardinera “Tram”',
+    description: '<p>Santa & Cole ®</p><p>Jaume Artigues, Miquel Roig, arquitectes</p><p>1993-94</p>',
+  },
+  {
+    src: 'images/portada/5 N7X489192v1.jpg',
+    title: 'Jardins del Casc Antic, Pou de la Figuera',
+    description: '<p>Ajuntament de Barcelona, Foment de Ciutat Vella SA</p><p>Jaume Artigues, Pere Riera, arquitectes (GRECCAT SL)</p><p>Matteo Caravatti (Arquitectes sense Fronteres)</p><p>2006-07</p>',
+  },
+  {
+    src: 'images/portada/1718_3 copia.jpg',
+    title: '12 habitatges als carrers de Riera Alta 15 i del Carme 104',
+    description: '<p>Barcelona (Districte de Ciutat Vella)</p><p>Jaume Artigues i Vidal, col.laborador Stefano Cortellaro</p><p>Seleccionat pel Concurs restringit i encàrrec del projecte</p><p>2001-03</p>',
+  },
+  {
+    src: 'images/portada/Facana dia.JPG',
+    title: 'Teatre Clavé de Tordera',
+    description: '<p>Ajuntament de Tordera (Barcelona)</p><p>“ARR, arquitectes, s.c.p.”: Jaume Artigues, Miquel Roig, Jordi Romero</p><p>1988-99</p>',
+  },
+];
