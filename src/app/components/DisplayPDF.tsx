55628a0196f218bfdc9010f17dabab4e793254b8
Aral Roca <aral-rg@hotmail.com>
2023-05-15 13:03:05 +0200

    Fix fallback PDF preview

--- src/app/components/DisplayPDF.tsx
+++ src/app/components/DisplayPDF.tsx
@@ -11,15 +11,16 @@ type Props = {
 export default function DisplayPDF({ pdfLink, page = 1, section = 20, name }: Props) {
   const controls = `#toolbar=0&navpanes=0&scrollbar=${page}&page=1&view=fitH,${section}`
   const src = pdfLink + controls
+  const style = {
+    width: '100%',
+    height: 'var(--main-max-height)',
+    marginTop: '-30px'
+  }
 
   return (
     <>
-      <object data={src} type="application/pdf" style={{
-        width: '100%',
-        height: 'var(--main-max-height)',
-        marginTop: '-30px'
-      }}>
-        <p>No és possible visualitzar el PDF.</p>
+      <object data={src} type="application/pdf" style={style}>
+        <embed src={src} type="application/pdf" style={style} />
       </object>
       <div style={{ textAlign: 'right' }}>
         <Link style={{ fontSize: 12 }} href={`/descarregar?pdf=${pdfLink}&name=${name}`} download>DESCARREGAR PDF</Link>
